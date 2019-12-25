434e2879937f7f0d4a7782a2b7d630489e4c677d
Dion Aditya <dionjatingaleh@gmail.com>
2019-12-25 20:01:27 +0700

    init

---
+++ index.js
@@ -0,0 +1,14 @@
+// @flow
+import React, { useEffect, useReducer } from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+	return(
+		<div>
+			Hello World
+		</div>
+		)
+}
+
+const mountNode = document.getElementById("app");
+
